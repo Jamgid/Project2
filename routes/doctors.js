6599fbfdcfa799434daf50160bed5bcbfb162403
bettycode <betty.haile.us@gmail.com>
2021-01-05 20:51:35 -0500

    modified

--- routes/doctors.js
+++ routes/doctors.js
@@ -23,7 +23,7 @@ db.findAll()
 router.get('/api/doctors', function(req, res){
 
     db.findAll({
-      
+        
     }).then(function(doctors){
     
     res.json(doctors)
@@ -33,6 +33,23 @@ router.get('/api/doctors', function(req, res){
 
 });
 
+//
+router.get('/api/doctors/:id', function(req, res){
+
+    db.findOne({
+        where:{
+            id:req.params.id
+        }
+        
+    }).then(function(doctors){
+    
+    res.json(doctors)
+    console.log(doctors)
+    
+});
+
+});
+//
 router.get('/welcome', (req, res) => res.render('welcome'));
 
 router.get('/add', (req, res) => res.render('add'));
